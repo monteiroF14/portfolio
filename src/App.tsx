af0a197b3a6dfeea675a45badd05217e0d64d3d4
André Monteiro <119819402+monteiroF14@users.noreply.github.com>
2024-01-25 02:22:03 +0000

    feat: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { env } from "./env";
+import { name, version } from "../package.json";
+
 export function App() {
+  document.title = `${env.VITE_GITHUB_API_USER}'s ${name}`;
+
   return (
     <>
-      <h1>Hello, World</h1>
-      env: {import.meta.env.VITE_APP_TITLE}
+      <h1 className="text-3xl font-bold underline font-inter">
+        {name}: Hello, World
+      </h1>
+      env: {env.VITE_GITHUB_API_TOKEN}
+      version: {version}
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
+import tailwindcss from "tailwindcss";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
   server: {
     port: 5000,
   },
