29ade454f42af3dc3916a6fa350aa18adad5c146
André Monteiro <amonteiro@e-goi.com>
2024-06-18 10:06:19 +0100

    refactor: update deploy and checkout actions

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
 	base: "/portfolio/",
+	build: {
+		outDir: "build",
+	},
 	css: {
 		postcss: {
 			plugins: [tailwindcss()],
