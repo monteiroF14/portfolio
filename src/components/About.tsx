bb3de5aee58ea098c988cb5b21aa78ff17993da3
André Monteiro <amonteiro@e-goi.com>
2024-06-18 10:00:47 +0100

    refactor: fix baseUrl and remove lucide-react

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,34 +1,16 @@
-import { Github, Linkedin } from "lucide-react";
-import { env } from "env";
-import { Link } from "react-router-dom";
-
 export function About() {
 	return (
 		<section className="h-screen flex flex-col justify-center gap-16 snap-center">
 			<div>
-				{/* <h2 className="font-inter text-5xl italic leading-none">about</h2> */}
-				<h1 className="leading-none text-9xl mb-4">{env.VITE_GITHUB_API_USER}</h1>
+				<h1 className="leading-none text-9xl mb-4">monteiroF14</h1>
 				<div className="flex gap-4 items-center">
 					<a
 						href="https://github.com/monteiroF14"
 						target="_blank"
 						className="drop-shadow-glow-sm hover:drop-shadow-glow-md"
 					>
-						<Github strokeWidth="2" size={48} />
+						<img src="assets/github/github-mark-white.svg" alt="Github Logo" className="w-12" />
 					</a>
-					<a
-						href="https://github.com/monteiroF14"
-						target="_blank"
-						className="drop-shadow-glow-sm  hover:drop-shadow-glow-md"
-					>
-						<Linkedin strokeWidth="2" size={48} fill="white" />
-					</a>
-					<Link
-						to={"/portfolio/awesome"}
-						className="drop-shadow-glow-sm  hover:drop-shadow-glow-md"
-					>
-						<button className="text-xl">awesome</button>
-					</Link>
 				</div>
 			</div>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/portfolio/",
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
-  server: {
-    port: 5000,
-  },
+	plugins: [react(), tsconfigPaths()],
+	base: "/portfolio/",
+	css: {
+		postcss: {
+			plugins: [tailwindcss()],
+		},
+	},
+	server: {
+		port: 5000,
+	},
 });
